e1c2273e77c0dec4c839e4f23ddf98910e5f1189
Gururaj2004 <guruguru6631@gmail.com>
2024-08-03 02:04:10 +0530

    production build

--- server.js
+++ server.js
@@ -9,9 +9,23 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 7001;
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://book-bug-client-host-62fqyd6cq-gururajm1s-projects.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
   })
 );
 
